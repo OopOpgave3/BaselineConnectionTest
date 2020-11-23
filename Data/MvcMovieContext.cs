06b139c8ddc97099f91ba12dd7161081a6f47dcf
simon sinø. nørregarrd <sin@pc-tec.local>
2020-11-23 12:27:24 +0100

    Add project files.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IntroduktionAspCore_MVC.Models;
+
+namespace MvcMovie.Data
+{
+    public class IntroduktionAspCore_MVC : DbContext
+    {
+        public IntroduktionAspCore_MVC(DbContextOptions<IntroduktionAspCore_MVC> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
